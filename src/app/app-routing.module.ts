bb5bb27585a852618aab72d577d759608574c7e0
Prashant Cholachagudda <pvc@outlook.com>
2019-06-11 13:30:50 +0530

    Added the new todo page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: 'new-todo', loadChildren: './new-todo/new-todo.module#NewTodoPageModule' },
 ];
 
 @NgModule({
---
+++ src/app/new-todo/new-todo.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-todo',
+  templateUrl: './new-todo.page.html',
+  styleUrls: ['./new-todo.page.scss'],
+})
+export class NewTodoPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
